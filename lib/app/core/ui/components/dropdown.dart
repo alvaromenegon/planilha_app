6fda03030070635b4510a4324893e303ee42dc51
Alvaro Eduardo Menegon Rosario <104403541+alvaromenegon@users.noreply.github.com>
2024-06-19 17:01:23 +0100

    correção do dropdown

--- lib/app/core/ui/components/dropdown.dart
+++ lib/app/core/ui/components/dropdown.dart
@@ -8,7 +8,6 @@ class Dropdown extends StatelessWidget {
   final List<String> items;
   final String value;
   final void Function(String?) onChanged;
-  final bool expand;
   final MainAxisAlignment mainAxisAlign;
   final CrossAxisAlignment crossAxisAlign;
 
@@ -17,12 +16,11 @@ class Dropdown extends StatelessWidget {
     required this.label,
     required this.items,
     required this.value,
-    this.expand = false,
     this.mainAxisAlign = MainAxisAlignment.spaceBetween,
     this.crossAxisAlign = CrossAxisAlignment.start,
     required this.onChanged,
   });
-
+/*
   _buildDropdownButton(
       List<String> items, String value, void Function(String?) onChanged) {
     return DropdownButton<String>(
@@ -35,54 +33,73 @@ class Dropdown extends StatelessWidget {
       padding: const EdgeInsets.only(left: 8.0, right: 8.0),
       items: items
           .map((String item) => DropdownMenuItem<String>(
-                enabled: item==''?false:true,
+                enabled: item == '' ? false : true,
                 value: item,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
                   child: Text(
-                    style: TextStyle(
-                      color: item==value?Colors.black:ColorsApp.instance.secondary,
-                    ),
-                    item),
+                      style: TextStyle(
+                        color: item == value
+                            ? Colors.black
+                            : ColorsApp.instance.secondary,
+                      ),
+                      item),
                 ),
               ))
           .toList(),
+    );
+  }*/
+  _buildDropdownButton(
+      List<String> items, String value, void Function(String?) onChanged) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return DropdownButton<String>(
+          icon: const Icon(Icons.arrow_drop_down_circle_outlined),
+          iconEnabledColor: ColorsApp.instance.primary,
+          value: value,
+          onChanged: onChanged,
+          underline: Container(),
+          borderRadius: ThemeConfig.defaultBorderRadius,
+          padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+          items: items
+              .map((String item) => DropdownMenuItem<String>(
+                    enabled: item == '' ? false : true,
+                    value: item,
+                    child: SizedBox(
+                      width: constraints.maxWidth-(8*5), //correção dos valores de padding
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            style: TextStyle(
+                              color: item == value
+                                  ? Colors.black
+                                  : ColorsApp.instance.secondary,
+                            ),
+                            item),
+                      ),
+                    ),
+                  ))
+              .toList(),
+        );
+      },
     );
   }
 
   @override
   Widget build(BuildContext context) {
-    if (!expand) {
-      return Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(
-            mainAxisAlignment: mainAxisAlign,
-            crossAxisAlignment: crossAxisAlign,
-            children: [
-              Label(label),
-              DecoratedBox(
+    return Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          mainAxisAlignment: mainAxisAlign,
+          crossAxisAlignment: crossAxisAlign,
+          children: [
+            Label(label),
+            DecoratedBox(
                 decoration: BoxDecoration(
                   borderRadius: ThemeConfig.defaultBorderRadius,
                   border: ThemeConfig.defaultBorder['border'] as Border,
                 ),
                 child: _buildDropdownButton(items, value, onChanged))
-            ],
-          ));
-    }
-    return Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Label(label),
-            DecoratedBox(
-                decoration: BoxDecoration(
-                  color: Colors.grey[200],
-                  borderRadius: BorderRadius.circular(8.0),
-                ),
-                child: _buildDropdownButton(items, value, onChanged))
-            //_buildDropdownButton(items, value, onChanged),
           ],
         ));
   }
